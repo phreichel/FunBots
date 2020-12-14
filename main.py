09a3528252e24b86740ec0c24aa028ff5b676aca
phreichel <40195890+phreichel@users.noreply.github.com>
2020-12-14 23:50:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 def welcome():
     print("Welcome to the chatter bot.")
+    print("Enter a sentence and expect a response.")
+    pass
+
+def input_line():
+    text = input(">")
+    print("You said: " + text)
+    return text
+
+def analyse(text):
     pass
 
 welcome()
+text = input_line()
+analyse(text)
